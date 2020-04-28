a87b36e075d880573c00c09f8b6652365d61a237
Marie Erin <m.oshaughnessy@outlook.com>
2020-04-28 10:58:29 -0400

    Updates

---
+++ precache-manifest.b644fa7c85cbb63b9592f753f4666a00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edfd0f0cefad127be04e519e69d8c0f",
+    "url": "/deployingit/index.html"
+  },
+  {
+    "revision": "fbd9ee40c0bdceb9650a",
+    "url": "/deployingit/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "96cb49c4b14de86f3723",
+    "url": "/deployingit/static/js/2.2bf54af1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployingit/static/js/2.2bf54af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbd9ee40c0bdceb9650a",
+    "url": "/deployingit/static/js/main.8b8dcc0f.chunk.js"
+  },
+  {
+    "revision": "c9722827f3ec6d91d93f",
+    "url": "/deployingit/static/js/runtime-main.e16302b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deployingit/static/media/logo.5d5d9eef.svg"
+  }
+]);
